57dcb14e0293a9f06fdb83ec6b682af166e43761
Oleg Plema <olegplema01@gmail.com>
2025-05-05 11:06:08 +0300

    feat: Code generation response

--- Back/Diagrams/src/main/java/org/plema/controllers/DiagramController.java
+++ Back/Diagrams/src/main/java/org/plema/controllers/DiagramController.java
@@ -1,10 +1,8 @@
 package org.plema.controllers;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
-import org.plema.CodeRunner;
+import org.plema.dtos.CodeResponse;
 import org.plema.dtos.MessageResponse;
 import org.plema.models.Diagram;
 import org.plema.services.GenerateCodeService;
@@ -14,12 +12,15 @@ public class DiagramController {
 
     private final GenerateCodeService generateCodeService = new GenerateCodeService();
     private final RunDiagramService runDiagramService = new RunDiagramService();
-    private final CodeRunner codeRunner = new CodeRunner();
 
     public void generateCode(RoutingContext context) {
         try {
             Diagram diagram = context.get("convertedData");
-            System.out.println(generateCodeService.generateCode(diagram));
+            String code = generateCodeService.generateCode(diagram);
+            context.response()
+                    .setStatusCode(201)
+                    .putHeader("Content-Type", "application/json")
+                    .end(JsonObject.mapFrom(new CodeResponse("Code generated", code)).encode());
         } catch (Exception e) {
             e.printStackTrace();
             context.response()
@@ -27,19 +28,10 @@ public class DiagramController {
                     .putHeader("Content-Type", "application/json")
                     .end(JsonObject.mapFrom(new MessageResponse("Something went wrong")).encode());
         }
-
-        context.response()
-                .setStatusCode(201)
-                .putHeader("Content-Type", "application/json")
-                .end(JsonObject.mapFrom(new MessageResponse("Code generated")).encode());
     }
 
     public void runDiagram(RoutingContext context) {
         try {
-//            JsonNode blocks = new ObjectMapper().readTree(context.body().asString());
-//            JsonNode threads = blocks.get("threads");
-//            JsonNode firstThread = threads.get(0);
-//            codeRunner.runBlocks(firstThread);
             Diagram diagram = context.get("convertedData");
             runDiagramService.runDiagram(diagram);
         } catch (Exception e) {
---
+++ Back/Diagrams/src/main/java/org/plema/dtos/CodeResponse.java
@@ -0,0 +1,4 @@
+package org.plema.dtos;
+
+public record CodeResponse(String message, String code) {
+}
